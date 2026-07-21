c3903aeaa907cdcecc0c8b075df05e1837100bdd
silvanynho <silvanynho@gmail.com>
2026-07-21 16:24:47 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'telas/dashboard.dart';
 import 'telas/login.dart';
 
+// ESSA PARTE É OBRIGATÓRIA E ESTAVA FALTANDO!
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const NutriControlApp());
+  runApp(const MyApp());
 }
 
-class NutriControlApp extends StatelessWidget {
-  const NutriControlApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nutri Control',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
         useMaterial3: true,
       ),
-      home: const VerificarLogin(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class VerificarLogin extends StatelessWidget {
-  const VerificarLogin({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(body: Center(child: CircularProgressIndicator()));
-        }
-        if (snapshot.hasData) {
-          return const Dashboard();
-        }
-        return const TelaLogin();
-      },
+      home: const TelaLogin(),
     );
   }
 }
