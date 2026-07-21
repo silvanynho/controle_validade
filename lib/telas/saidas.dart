f05f6395b0457736868f85bfb8d9235d611d94ae
silvanynho <silvanynho@gmail.com>
2026-07-21 15:48:18 -0300

    Update saidas.dart

--- lib/telas/saidas.dart
+++ lib/telas/saidas.dart
@@ -20,7 +20,7 @@ class _TelaSaidaState extends State<TelaSaida> {
     try {
       var resultado = await BarcodeScanner.scan();
       if (resultado.raw.isNotEmpty) {
-        setState(() => _codigo.text = resultado.text);
+        setState(() => _codigo.text = resultado.rawContent);
         _buscarProduto();
       }
     } catch (_) {}
