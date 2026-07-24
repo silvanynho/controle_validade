95eae93b4bbe2394ad27b43d370d8a6809297d1f
silvanynho <silvanynho@gmail.com>
2026-07-24 09:54:44 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'telas/login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  try {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AlzaSyCjVdtlcIByb9H6DJTOkjSw-17CTSR6hwO",
+        appId: "1677339036980:android:dfa4957b23b406c16c7961",
+        messagingSenderId: "677339036980",
+        projectId: "nutri-control-2a34d",
+      ),
+    );
+  } catch (e) {
+    debugPrint("Erro ao inicializar o Firebase: $e");
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Nutri Control",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: false,
+      ),
+      home: const TelaLogin(),
+    );
+  }
+}
